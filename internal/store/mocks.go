b004a2c37f83b463a3e9598bb0dccb3bbeeadb23
Lelouchlamperougexd <119339442+Lelouchlamperougexd@users.noreply.github.com>
2026-03-02 05:15:17 +0500

    Complete MockStore

--- internal/store/mocks.go
+++ internal/store/mocks.go
@@ -8,9 +8,15 @@ import (
 
 func NewMockStore() Storage {
 	return Storage{
-		Users:       &MockUserStore{},
-		LoginEvents: &MockLoginEventStore{},
-		Companies:   &MockCompanyStore{},
+		Users:        &MockUserStore{},
+		LoginEvents:  &MockLoginEventStore{},
+		Companies:    &MockCompanyStore{},
+		Roles:        &MockRoleStore{},
+		Projects:     &MockProjectStore{},
+		Listings:     &MockListingStore{},
+		Applications: &MockApplicationStore{},
+		Messages:     &MockMessageStore{},
+		Invites:      &MockInviteStore{},
 	}
 }
 
@@ -70,4 +76,88 @@ func (m *MockCompanyStore) UpdateVerificationStatus(ctx context.Context, id int6
 
 func (m *MockCompanyStore) List(ctx context.Context, fq PaginatedFeedQuery) ([]Company, error) {
 	return []Company{}, nil
+}
+
+type MockRoleStore struct{}
+
+func (m *MockRoleStore) GetByName(ctx context.Context, name string) (*Role, error) {
+	return &Role{Name: name}, nil
+}
+
+type MockProjectStore struct{}
+
+func (m *MockProjectStore) Create(ctx context.Context, project *Project) error {
+	return nil
+}
+
+func (m *MockProjectStore) ListByCompany(ctx context.Context, companyID int64) ([]Project, error) {
+	return []Project{}, nil
+}
+
+func (m *MockProjectStore) GetByID(ctx context.Context, id int64) (*Project, error) {
+	return &Project{ID: id}, nil
+}
+
+type MockListingStore struct{}
+
+func (m *MockListingStore) Create(ctx context.Context, listing *Listing, media []ListingMedia, rent *RentConstraints) error {
+	return nil
+}
+
+func (m *MockListingStore) UpdateStatus(ctx context.Context, id int64, status string) error {
+	return nil
+}
+
+func (m *MockListingStore) GetByID(ctx context.Context, id int64) (*Listing, error) {
+	return &Listing{ID: id}, nil
+}
+
+func (m *MockListingStore) List(ctx context.Context, filter ListingFilter) ([]Listing, error) {
+	return []Listing{}, nil
+}
+
+type MockApplicationStore struct{}
+
+func (m *MockApplicationStore) Create(ctx context.Context, app *Application) error {
+	return nil
+}
+
+func (m *MockApplicationStore) UpdateStatus(ctx context.Context, id int64, status string) error {
+	return nil
+}
+
+func (m *MockApplicationStore) GetByID(ctx context.Context, id int64) (*Application, error) {
+	return &Application{ID: id}, nil
+}
+
+func (m *MockApplicationStore) List(ctx context.Context, filter ApplicationFilter) ([]Application, error) {
+	return []Application{}, nil
+}
+
+func (m *MockApplicationStore) GetByListingAndUser(ctx context.Context, listingID, userID int64) (*Application, error) {
+	return nil, ErrNotFound
+}
+
+type MockMessageStore struct{}
+
+func (m *MockMessageStore) Create(ctx context.Context, msg *ApplicationMessage) error {
+	return nil
+}
+
+func (m *MockMessageStore) List(ctx context.Context, applicationID int64, limit, offset int) ([]ApplicationMessage, error) {
+	return []ApplicationMessage{}, nil
+}
+
+type MockInviteStore struct{}
+
+func (m *MockInviteStore) Create(ctx context.Context, invite *RegistrationInvite) error {
+	return nil
+}
+
+func (m *MockInviteStore) GetByToken(ctx context.Context, token string) (*RegistrationInvite, error) {
+	return nil, ErrInviteNotFound
+}
+
+func (m *MockInviteStore) MarkUsed(ctx context.Context, id int64) error {
+	return nil
 }
