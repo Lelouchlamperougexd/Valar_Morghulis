b91302d676c903373b7990532b6e58f9ffc775be
Lelouchlamperougexd <119339442+Lelouchlamperougexd@users.noreply.github.com>
2026-03-02 05:36:02 +0500

    dead fields (Sort, Tags, Search, Since, Until) and parseTime removed

--- internal/store/pagination.go
+++ internal/store/pagination.go
@@ -3,78 +3,35 @@ package store
 import (
 	"net/http"
 	"strconv"
-	"strings"
-	"time"
 )
 
-type PaginatedFeedQuery struct {
-	Limit  int      `json:"limit" validate:"gte=1,lte=20"`
-	Offset int      `json:"offset" validate:"gte=0"`
-	Sort   string   `json:"sort" validate:"oneof=asc desc"`
-	Tags   []string `json:"tags" validate:"max=5"`
-	Search string   `json:"search" validate:"max=100"`
-	Since  string   `json:"since"`
-	Until  string   `json:"until"`
+// PaginatedQuery holds generic pagination parameters.
+type PaginatedQuery struct {
+	Limit  int `json:"limit" validate:"gte=1,lte=20"`
+	Offset int `json:"offset" validate:"gte=0"`
 }
 
-func (fq PaginatedFeedQuery) Parse(r *http.Request) (PaginatedFeedQuery, error) {
+// PaginatedFeedQuery is kept as an alias for backward-compatibility.
+type PaginatedFeedQuery = PaginatedQuery
+
+func (pq PaginatedQuery) Parse(r *http.Request) (PaginatedQuery, error) {
 	qs := r.URL.Query()
 
-	limit := qs.Get("limit")
-	if limit != "" {
-		l, err := strconv.Atoi(limit)
+	if v := qs.Get("limit"); v != "" {
+		l, err := strconv.Atoi(v)
 		if err != nil {
-			return fq, nil
+			return pq, nil
 		}
-
-		fq.Limit = l
+		pq.Limit = l
 	}
 
-	offset := qs.Get("offset")
-	if offset != "" {
-		l, err := strconv.Atoi(offset)
+	if v := qs.Get("offset"); v != "" {
+		o, err := strconv.Atoi(v)
 		if err != nil {
-			return fq, nil
+			return pq, nil
 		}
-
-		fq.Offset = l
+		pq.Offset = o
 	}
 
-	sort := qs.Get("sort")
-	if sort != "" {
-		fq.Sort = sort
-	}
-
-	tags := qs.Get("tags")
-	if tags != "" {
-		fq.Tags = strings.Split(tags, ",")
-	} else {
-		fq.Tags = []string{}
-	}
-
-	search := qs.Get("search")
-	if search != "" {
-		fq.Search = search
-	}
-
-	since := qs.Get("since")
-	if since != "" {
-		fq.Since = parseTime(since)
-	}
-
-	until := qs.Get("until")
-	if until != "" {
-		fq.Until = parseTime(until)
-	}
-
-	return fq, nil
-}
-
-func parseTime(s string) string {
-	t, err := time.Parse(time.DateTime, s)
-	if err != nil {
-		return ""
-	}
-
-	return t.Format(time.DateTime)
+	return pq, nil
 }
